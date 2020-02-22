18251fb30e38f6588dc901045200467657b90aaf
ytker <48038411@qq.com>
2020-02-23 00:42:54 +0800

    郭瑞昌更新了函数式编程练习，并且增加了非抽象方法

--- src/main/java/cn/soft1851/practice/guo/BookService.java
+++ src/main/java/cn/soft1851/practice/guo/BookService.java
@@ -1,5 +1,16 @@
 package cn.soft1851.practice.guo;
 @FunctionalInterface
 public interface BookService {
-    void sellBook(String name);
+    void sellBook(String name,Double price);
+
+    /**
+     * 编写静态方法，不算做抽象方法
+     * @param name
+     * @param price
+     */
+    static void calPrice(String name,Double price) {
+        if (name == "郭瑞昌"){
+            System.out.println("价格为"+price * 0.5);
+        }
+    }
 }
--- src/main/java/cn/soft1851/practice/guo/ServiceTest.java
+++ src/main/java/cn/soft1851/practice/guo/ServiceTest.java
@@ -13,9 +13,11 @@ public class ServiceTest {
             System.out.print(peopleName+"购买了一本");
         };
         peopleService.peopleName("郭瑞昌");
-        BookService bookService = name -> {
+        BookService bookService = (name,price) -> {
             System.out.println(name+"书");
         };
-        bookService.sellBook("java");
+        bookService.sellBook("java",66.00);
+        //调用静态方法
+        BookService.calPrice("郭瑞昌",66.00);
     }
 }
